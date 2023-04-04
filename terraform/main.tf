b399e4b9f71abae97d9b780f37c289eae29c4af6
tonireti <tobionireti@gmail.com>
2023-04-04 07:41:58 -0400

    New update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,93 +1,54 @@
-# Copyright 2022 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# Definition of local variables
-locals {
-  base_apis = [
-    "container.googleapis.com",
-    "monitoring.googleapis.com",
-    "cloudtrace.googleapis.com",
-    "cloudprofiler.googleapis.com"
-  ]
-  memorystore_apis = ["redis.googleapis.com"]
-  cluster_name     = google_container_cluster.my_cluster.name
+provider "google" {
+    project = "kubernetes-projects-381902"
+    credentials = "${file("credentials.json")}"
+    region = "us-central1"
 }
 
-# Enable Google Cloud APIs
-module "enable_google_apis" {
-  source  = "terraform-google-modules/project-factory/google//modules/project_services"
-  version = "~> 14.0"
+resource "google_container_cluster" "onlineboutique_cluster" {
+    name        = "onlineboutique-cluster"
+    location    = "us-central1"
+    node_locations = [ "us-central1-a","us-central1-b","us-central1-c" ]
+    initial_node_count = 1
+    addons_config {
+        http_load_balancing {
+            disabled = true
+        }
+        horizontal_pod_autoscaling {
+            disabled = false
+        }
+    }
+    release_channel {
+      channel = "REGULAR"
+    }
+    workload_identity_config {
+      workload_pool = "kubernetes-projects-381902.svc.id.goog"
+    }
+    
+    private_cluster_config {
+      enable_private_nodes = false
+      enable_private_endpoint = false
+    }
 
-  project_id                  = var.gcp_project_id
-  disable_services_on_destroy = false
+    cluster_autoscaling {
+      auto_provisioning_defaults {
+        management {
+          auto_repair = true
+          auto_upgrade = true
+        }
+      }
+    }
+    node_config {
+      preemptible = true
+      machine_type = "e2-standard-2"
+      oauth_scopes = [
+        "https://www.googleapis.com/auth/compute",
+        "https://www.googleapis.com/auth/devstorage.read_only",
+        "https://www.googleapis.com/auth/logging.write",
+        "https://www.googleapis.com/auth/monitoring.write",
+        "https://www.googleapis.com/auth/servicecontrol",
+        "https://www.googleapis.com/auth/service.management.readonly",
+        "https://www.googleapis.com/auth/trace.append",
+      ]
+    }
 
-  # activate_apis is the set of base_apis and the APIs required by user-configured deployment options
-  activate_apis = concat(local.base_apis, var.memorystore ? local.memorystore_apis : [])
-}
-
-# Create GKE cluster
-resource "google_container_cluster" "my_cluster" {
-
-  name     = var.name
-  location = var.region
-
-  # Enabling autopilot for this cluster
-  enable_autopilot = true
-
-  # Setting an empty ip_allocation_policy to allow autopilot cluster to spin up correctly
-  ip_allocation_policy {
-  }
-
-  depends_on = [
-    module.enable_google_apis
-  ]
-}
-
-# Get credentials for cluster
-module "gcloud" {
-  source  = "terraform-google-modules/gcloud/google"
-  version = "~> 3.0"
-
-  platform              = "linux"
-  additional_components = ["kubectl", "beta"]
-
-  create_cmd_entrypoint = "gcloud"
-  # Module does not support explicit dependency
-  # Enforce implicit dependency through use of local variable
-  create_cmd_body = "container clusters get-credentials ${local.cluster_name} --zone=${var.region} --project=${var.gcp_project_id}"
-}
-
-# Apply YAML kubernetes-manifest configurations
-resource "null_resource" "apply_deployment" {
-  provisioner "local-exec" {
-    interpreter = ["bash", "-exc"]
-    command     = "kubectl apply -k ${var.filepath_manifest} -n ${var.namespace}"
-  }
-
-  depends_on = [
-    module.gcloud
-  ]
-}
-
-# Wait condition for all Pods to be ready before finishing
-resource "null_resource" "wait_conditions" {
-  provisioner "local-exec" {
-    interpreter = ["bash", "-exc"]
-    command     = "kubectl wait --for=condition=ready pods --all -n ${var.namespace} --timeout=-1s 2> /dev/null"
-  }
-
-  depends_on = [
-    resource.null_resource.apply_deployment
-  ]
 }
